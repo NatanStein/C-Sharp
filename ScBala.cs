efc21dd49304aa598bdb3c24e333e128b901aea0
Natan Steinbruch <51870564+NatanStein@users.noreply.github.com>
2019-09-10 13:18:03 -0300

    Add files via upload

---
+++ ScBala.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScBala : MonoBehaviour
+{
+    public Rigidbody2D rd2d;
+    public float v;
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        Destroy(gameObject,2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rd2d.velocity = transform.right * v;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag != "NoDestroy")
+        {
+            Destroy(collision.transform.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ movepos.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movepos : MonoBehaviour
+{
+    public float velocidade,fpulo;
+    public Rigidbody2D rd2d;
+    public bool noChao;
+    public GameObject bala;
+    public Transform arma;
+    Collider2D myCollider;
+
+
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent < Collider2D >();
+        noChao = false;
+    }
+    void Update()
+    {
+        float velocidadeAtual = velocidade;
+        myCollider.enabled = false;
+        if (Physics2D.Raycast(transform.position,Vector3.down,1.5f))
+        {
+            noChao = true;
+        }
+        else
+        {
+            noChao = false;
+        }
+        myCollider.enabled = true;
+        if (Input.GetKey("a"))
+        {
+            rd2d.AddForce(Vector2.left * velocidade, ForceMode2D.Force);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rd2d.AddForce(Vector2.right * velocidade, ForceMode2D.Force);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && noChao)
+        {
+            rd2d.AddForce(Vector2.up * fpulo);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bala, arma.position, arma.rotation);
+        }
+    }
+}
